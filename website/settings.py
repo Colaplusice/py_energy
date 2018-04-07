a6c64454fbdc304bcc72fc056c0437441ecb0beb
Colaplusice <fjl2401@163.com>
2018-04-07 22:18:27 +0800

    sd

--- website/settings.py
+++ website/settings.py
@@ -139,11 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-<<<<<<< HEAD
+# <<<<<<< HEAD
 
-=======
->>>>>>> a627d79737072b043a74204d3b81ac18dffb63d4
-
+# =======
+# >>>>>>> a627d79737072b043a74204d3b81ac18dffb63d
 STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
